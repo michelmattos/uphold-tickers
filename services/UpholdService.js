11533fe77fcd2710d8ca555b875c7adedf5398f6
Michel Mattos <mattos.michel@gmail.com>
2016-05-14 00:11:36 -0300

    Created a new service: UpholdService

---
+++ services/UpholdService.js
@@ -0,0 +1,38 @@
+const request = require('request')
+const EventEmitter = require('events')
+
+function start (url, milliseconds) {
+    let emitter = new EventEmitter()
+    _initTimer(url, emitter, milliseconds)
+    return emitter
+}
+
+function _initTimer (url, emitter, milliseconds) {
+    return setTimeout(
+        _requestUphold(url, emitter),
+        milliseconds
+    )
+}
+
+function _requestUphold (url, emitter) {
+    let startTime = Date.now()
+    
+    request(
+        {
+            url: 'https://api.uphold.com/v0/ticker/USD',
+            json: true
+        },
+        (error, response, body) => {
+            let timeLapsed = Date.now() - startTime
+            
+            if (error)
+                emitter.emit('error', error)
+            else if (response.statusCode !== 200)
+                emitter.emit('failed', `Request failed with HTTP Status ${response.statusCode}.`, timeLapsed)
+            else if (response.statusCode == 200)
+                emitter.emit('success', body, timeLapsed)
+        }
+    )
+}
+
+module.exports = { start }
